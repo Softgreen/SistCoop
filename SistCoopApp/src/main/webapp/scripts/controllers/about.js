9638c5df321e9d39701c72e9ebbef79f6a75ce1b
carlos <carlosthe19916@gmail.com>
2014-07-31 17:47:01 -0500

    1
    1

--- SistCoopApp/src/main/webapp/scripts/controllers/about.js
+++ SistCoopApp/src/main/webapp/scripts/controllers/about.js
@@ -2,13 +2,13 @@
 
 /**
  * @ngdoc function
- * @name mainApp.controller:MainCtrl
+ * @name mainApp.controller:AboutCtrl
  * @description
- * # MainCtrl
+ * # AboutCtrl
  * Controller of the mainApp
  */
 angular.module('mainApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('AboutCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
--- SistCoopApp/src/main/webapp/scripts/controllers/caja/VoucherTransaccionCajaCajaController.js
+++
@@ -1,8 +0,0 @@
-define(['../module'], function (controllers) {
-    'use strict';
-    controllers.controller('VoucherTransaccionCajaCajaController', ['$scope', "$state", '$filter', "MonedaService", "CajaSessionService",
-        function($scope, $state, $filter, MonedaService,CajaSessionService) {
-
-
-        }]);
-});
