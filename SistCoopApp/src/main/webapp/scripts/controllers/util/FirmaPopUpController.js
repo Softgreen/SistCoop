fc4343eb1df0251dc0ff8943831cd50085ae5232
carlos <carlosthe19916@gmail.com>
2014-07-31 18:14:54 -0500

    angular

---
+++ SistCoopApp/src/main/webapp/scripts/controllers/util/FirmaPopUpController.js
@@ -0,0 +1,23 @@
+define(['../module'], function (controllers) {
+    'use strict';
+    controllers.controller('FirmaPopUpController', function ($scope, $modalInstance, idPersonas, nombres) {
+
+        $scope.idPersonas = idPersonas;
+        $scope.nombres = nombres;
+
+        $scope.getUrlFirma = function(index) {
+            return "http://localhost:8080/SistemaFinancieroVentura-web/services/personaNatural/"+$scope.idPersonas[index]+"/firma";
+        };
+        $scope.getNombre = function(index){
+            return $scope.nombres[index];
+        };
+
+        $scope.ok = function () {
+            $modalInstance.close();
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
+});
---
+++ SistCoopApp/src/main/webapp/scripts/controllers/util/LoadImageController.js
@@ -0,0 +1,43 @@
+define(['../module'], function (controllers) {
+    'use strict';
+
+    controllers.controller("LoadImageController", ["$scope",
+        function($scope) {
+
+            $scope.urlBase = "http://localhost:8080/SistemaFinancieroVentura-web/services/personaNatural/";
+            $scope.baseTarget = "/SistemaFinancieroVentura-web/services/personaNatural/";
+            $scope.idPersona = undefined;
+            $scope.tipoImagen = undefined;
+
+            //tipofoto ppuede ser FOTO O FIRMA
+            $scope.configImagen = function(id, tipoFoto){
+                $scope.tipoImagen = tipoFoto;
+                if(!angular.isUndefined(id)){
+                    return {
+                        singleFile: true,
+                        target: $scope.baseTarget + id + '/' + tipoFoto
+                    }
+                } else {
+                    return {
+                        singleFile: true,
+                        target: '/upload'
+                    }
+                }
+            };
+
+            $scope.getImagen = function(id,tipoFoto) {
+                if(!angular.isUndefined(id))
+                    return $scope.urlBase + id + "/" + tipoFoto;
+            };
+
+            $scope.$watch("view.id", function(){
+                if(!angular.isUndefined($scope.view)){
+                    if(!angular.isUndefined($scope.view.id)){
+                        $scope.idPersona = $scope.view.id;
+                        $scope.$flow.opts.target = $scope.baseTarget + $scope.idPersona + '/' + $scope.tipoImagen;
+                    }
+                }
+            });
+
+        }]);
+});
