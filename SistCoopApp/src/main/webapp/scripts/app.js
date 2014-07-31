0f53cdd702c006892cb69b6013f9ffe836d5a476
carlos <carlosthe19916@gmail.com>
2014-07-31 17:17:17 -0500

    Controllers

--- SistCoopApp/src/main/webapp/scripts/app.js
+++ SistCoopApp/src/main/webapp/scripts/app.js
@@ -1,33 +1,34 @@
-'use strict';
+/*jshint unused: vars */
+define([
+        'angular',
+        'uiRouter',
+        './controllers/main',
+        './directives/main',
+        './filters/main',
+        './services/main'
+    ]/*deps*/,
+    function (angular, MainCtrl)/*invoke*/ {
+        'use strict';
 
-/**
- * @ngdoc overview
- * @name webappApp
- * @description
- * # webappApp
- *
- * Main module of the application.
- */
-angular
-  .module('webappApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        return angular.module('cajaApp', [
+            'cajaApp.services',
+            'cajaApp.controllers',
+            'cajaApp.filters',
+            'cajaApp.directives',
+
+            /*angJSDeps*/
+            'ui.router',
+            'restangular',
+            'ngProgress',
+            'ui.bootstrap',
+            'ngGrid',
+            'ngSanitize',
+            'ngAnimate',
+            'ui.utils',
+            'blockUI',
+            'flow',
+            'focusOn',
+            'cfp.hotkeys'
+        ]);
+    }
+);
