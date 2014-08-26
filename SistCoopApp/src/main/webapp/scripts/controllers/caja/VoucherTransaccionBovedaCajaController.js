89a3275c788aa3328403a57be9db26443085d813
Jhon Flores <jfh_flores_11@hotmail.com>
2014-08-26 15:31:12 -0500

    123

--- SistCoopApp/src/main/webapp/scripts/controllers/caja/VoucherTransaccionBovedaCajaController.js
+++ SistCoopApp/src/main/webapp/scripts/controllers/caja/VoucherTransaccionBovedaCajaController.js
@@ -39,17 +39,25 @@ define(['../module'], function (controllers) {
                 qz.append(String.fromCharCode(27) + "\x61" + "\x31");							//texto centrado
                 qz.append("C.A.C. CAJA VENTURA \r\n");											// \r\n salto de linea
                 qz.append("TRANSACCION BOVEDA/CAJA " + "\r\n");
-                // \t tabulador
+                																				// \t tabulador
                 qz.append("\x1B\x21\x01");														//texto normal (no negrita)
                 qz.append(String.fromCharCode(27) + "\x61" + "\x30");							//texto a la izquierda
 
-                qz.append(($scope.transaccionBovedaCaja.agenciaAbreviatura) + "\t\t" + "TRANS:" + "\t" + ($scope.transaccionBovedaCaja.id) + "\r\n");
-                qz.append("CAJA:" + "\t" + ($scope.transaccionBovedaCaja.cajaDenominacion) + "\t\t" + "Nro OP:" + "\t" + "\r\n");
+                qz.append("AGENCIA:" + ($scope.transaccionBovedaCaja.agenciaAbreviatura) + "\r\n");
+                qz.append("TRANS:" + "\t" + ($scope.transaccionBovedaCaja.id) + "\r\n");
+                qz.append("ORIGEN:" + "\t" + ($scope.transaccionBovedaCaja.cajaDenominacion) + "(" + $scope.transaccionBovedaCaja.cajaAbreviatura + ")" + "\r\n");
+                qz.append("DESTINO:" + "BOVEDA " + ($scope.transaccionBovedaCaja.moneda.denominacion) + "\r\n");
                 qz.append("FECHA:" + "\t" + ($filter('date')($scope.transaccionBovedaCaja.fecha, 'dd/MM/yyyy')) + " " + ($filter('date')($scope.transaccionBovedaCaja.hora, 'HH:mm:ss')) + "\r\n");
                 qz.append("MONEDA:" + "\t" + ($scope.transaccionBovedaCaja.moneda.denominacion) + "(" + $scope.transaccionBovedaCaja.moneda.simbolo + ")" + "\r\n");
-                qz.append("ORIGEN:" + "\t" + ($scope.transaccionBovedaCaja.origen) + "\r\n");
                 qz.append("MONTO:" + "\t" + ($filter('currency')($scope.transaccionBovedaCaja.monto, $scope.transaccionBovedaCaja.moneda.simbolo)) + "\r\n");
-
+                qz.append("\r\n");
+                qz.append("\r\n");
+                qz.append("\r\n");
+                qz.append(String.fromCharCode(27) + "\x61" + "\x30");
+				qz.append("_______________" + "\t\t" + "_______________" + "\r\n");
+				qz.append(String.fromCharCode(27) + "\x61" + "\x30");
+                qz.append("Firma Cajero" + "\t\t" + "Firma Jefe Caja" + "\r\n");
+                
                 qz.append("\x1D\x56\x41");														//cortar papel
                 qz.append("\x1B\x40");
                 qz.print();
