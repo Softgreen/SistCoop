b716ec490e395f6454fca7cef8558c757d6b2afa
carlos <carlosthe19916@gmail.com>
2014-10-21 16:37:50 -0500

    Transaccion boveda boveda

--- SistCoopJefeCajaApp/src/main/webapp/scripts/controllers/transaccion/CrearTransaccionBovedaBovedaController.js
+++ SistCoopJefeCajaApp/src/main/webapp/scripts/controllers/transaccion/CrearTransaccionBovedaBovedaController.js
@@ -58,9 +58,46 @@ define(['../module'], function (controllers) {
             $scope.loadBovedasOrigen();
 
 
-            $scope.crearTransaccion = function() {
-                if($scope.formCrearTransaccionBovedaCaja.$valid){
+            $scope.getBovedaOrigen = function(){
+                if(!angular.isUndefined($scope.view.idBovedaOrigen)) {
+                    for(var i = 0; i < $scope.objetosCargados.bovedasOrigen.length; i++){
+                        if($scope.objetosCargados.bovedasOrigen[i].id == $scope.view.idBovedaOrigen){
+                            return $scope.objetosCargados.bovedasOrigen[i];
+                        }
+                    }
+                }
+            };
+            $scope.getBovedaDestino = function() {
+                if(!angular.isUndefined($scope.view.idBovedaDestino)) {
+                    for(var i = 0; i < $scope.objetosCargados.bovedasDestino.length; i++){
+                        if($scope.objetosCargados.bovedasDestino[i].id == $scope.view.idBovedaDestino){
+                            return $scope.objetosCargados.bovedasDestino[i];
+                        }
+                    }
+                }
+            };
 
+            $scope.crearTransaccion = function() {
+                var monedaOrigen = $scope.getBovedaOrigen();
+                var monedaDestino = $scope.getBovedaDestino();
+
+                if($scope.formCrearTransaccionBovedaCaja.$valid
+                    && $scope.view.idBovedaOrigen != $scope.view.idBovedaDestino
+                    && !angular.isUndefined(monedaOrigen) && !angular.isUndefined(monedaDestino)
+                    && monedaOrigen.moneda.id == monedaDestino.moneda.id){
+
+                    BovedaService.crearTransaccioBovedaBoveda($scope.view.idBovedaOrigen, $scope.view.idBovedaDestino, $scope.objetosCargados.detalleBovedaOrigen).then(
+                        function(data){
+                            $state.transitionTo('app.transaccion.voucherTransaccionBovedaBoveda', {id: data.id});
+                            $scope.control.inProcess = false;
+                        },
+                        function error(error){
+                            $scope.control.inProcess = false;
+                            $scope.control.success = false;
+                            $scope.alerts = [{ type: "danger", msg: "Error: " + error.data.message + "."}];
+                            $scope.closeAlert = function(index) {$scope.alerts.splice(index, 1);};
+                        }
+                    );
                 } else {
                     $scope.control.submitted = true;
                 }
