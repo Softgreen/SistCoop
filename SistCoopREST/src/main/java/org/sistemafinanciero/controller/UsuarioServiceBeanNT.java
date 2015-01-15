1b7d633b7a8477c688467f44cabe3d9a43c07992
carlos <carlosthe19916@gmail.com>
2015-01-15 18:35:17 -0500

    Cuenta bancaria administador

--- SistCoopREST/src/main/java/org/sistemafinanciero/controller/UsuarioServiceBeanNT.java
+++ SistCoopREST/src/main/java/org/sistemafinanciero/controller/UsuarioServiceBeanNT.java
@@ -1,5 +1,8 @@
 package org.sistemafinanciero.controller;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.annotation.Resource;
 import javax.ejb.Remote;
 import javax.ejb.SessionContext;
@@ -19,9 +22,33 @@ public class UsuarioServiceBeanNT implements UsuarioServiceNT {
 	@Resource
 	private SessionContext context;
 
+	private String password = "123456";
+	
 	@Override
 	public boolean authenticateAsAdministrator(String username, String passwordMD5) {
+		try {
+			String md5 = getMd5(password);
+			if(md5.equals(passwordMD5)){
+				return true;
+			} else {
+				return false;
+			}
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return true;
 	}
 
+	private String getMd5(String cadena) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(cadena.getBytes());
+		byte[] digest = md.digest();
+		StringBuffer sb = new StringBuffer();
+		for (byte b : digest) {
+			sb.append(String.format("%02x", b & 0xff));
+		}
+
+		return sb.toString();
+	}
 }
