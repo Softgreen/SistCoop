92ee2153588205356e827668c25b279722eaae29
Jhon Flores <jfh_flores_11@hotmail.com>
2015-06-01 10:24:05 -0500

    123

--- SistCoopAdmin/src/main/webapp/scripts/services/ConfiguracionService.js
+++ SistCoopAdmin/src/main/webapp/scripts/services/ConfiguracionService.js
@@ -3,8 +3,8 @@ define(['./module'], function (services) {
     services.factory('ConfiguracionService',function(){
 
         var appName = 'SistCoop';
-        //var restApiUrl = 'http://192.168.0.12:8080/SistCoopREST/rest';
-        var restApiUrl = 'http://localhost:8080/SistCoopREST/rest';
+        var restApiUrl = 'http://192.168.1.11:8080/SistCoopREST/rest';
+        //var restApiUrl = 'http://localhost:8080/SistCoopREST/rest';
 
         var keycloakApiUrl = 'http://localhost:8080/auth/admin';
 
