07e53c387f3aa7b3a46ffc52a685e887376242bd
Carlos <carlosthe19916@gmail.com>
2015-06-17 13:01:07 -0500

    enviar giro

---
+++ SistCoopApp/src/main/webapp/scripts/controllers/caja/transaccion/DesembolsarTransaccionGiroController.js
@@ -0,0 +1,9 @@
+define(['../../module'], function (controllers) {
+  'use strict';
+  controllers.controller('DesembolsarTransaccionGiroController', ["$scope", "$state", "PersonaNaturalService", "AgenciaService", "SessionService", "MonedaService",
+    function ($scope, $state, PersonaNaturalService, AgenciaService, SessionService, MonedaService) {
+
+
+
+    }]);
+});
