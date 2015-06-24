8e75b1f5d6d8e51f239a4da637d5a586e015711f
Carlos <carlosthe19916@gmail.com>
2015-06-24 09:50:35 -0500

    transaccion giro

--- SistCoopApp/src/main/webapp/scripts/controllers/caja/transaccion/BuscarGirosController.js
+++ SistCoopApp/src/main/webapp/scripts/controllers/caja/transaccion/BuscarGirosController.js
@@ -33,15 +33,27 @@ define(['../../module'], function (controllers) {
 
           {field: "comision", displayName: 'Comision', cellFilter: "currency: ''", width: 60},
           {field: "lugarPagoComision", displayName: 'Comision', width: 110},
-          {displayName: 'Comision', cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estadoPagoComision">PAGADO</span><span ng-hide="row.entity.estadoPagoComision">NO PAGADO</span></div>', width:80},
+          {displayName: 'Comision', cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estadoPagoComision">PAGADO</span><span ng-hide="row.entity.estadoPagoComision">NO PAGADO</span></div>', width: 95},
 
           {field: "fechaEnvio", cellFilter: "date : 'dd/MM/yyyy'", displayName: 'F.Envio', width: 70},
           {field: "fechaDesembolso", cellFilter: "date : 'dd/MM/yyyy'", displayName: 'F.Desembolso', width: 70},
-          {field: "estado", displayName: 'Estado', width: 80},
+          {field: "estado", displayName: 'Estado', width: 95},
           {
             displayName: 'Edit',
-            width: 170,
-            cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><button type="button" class="btn btn-info btn-xs" ng-click="getVoucher(row.entity)" ng-disabled="getDisabledStateVoucher(row.entity)"><span class="glyphicon glyphicon-share"></span>Voucher</button>&nbsp;<button type="button" class="btn btn-success btn-xs" ng-click="confirmarTransaccion(row.entity)" ng-disabled="getDisabledStateButton(row.entity)"><span class="glyphicon glyphicon-ok"></span>Confirmar</button></div>'
+            width: 135,
+            cellTemplate:
+            '<div class="btn-group dropdown">' +
+            '<button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">' +
+            'Button<span class="caret"></span>' +
+            '</button>' +
+            '<ul class="dropdown-menu">' +
+            '<li><a href="#">Action</a></li>' +
+            '<li><a href="#">Another action</a></li>' +
+            '<li><a href="#">Something else here</a></li>' +
+            '<li class="divider"></li>' +
+            '<li><a href="#">Separated link</a></li>' +
+            '</ul>' +
+            '</div>'
           }]
       };
 
@@ -57,15 +69,31 @@ define(['../../module'], function (controllers) {
 
           {field: "comision", displayName: 'Comision', cellFilter: "currency: ''", width: 60},
           {field: "lugarPagoComision", displayName: 'Comision', width: 110},
-          {displayName: 'Comision', cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estadoPagoComision">PAGADO</span><span ng-hide="row.entity.estadoPagoComision">NO PAGADO</span></div>', width: 80},
+          {displayName: 'Comision', cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estadoPagoComision">PAGADO</span><span ng-hide="row.entity.estadoPagoComision">NO PAGADO</span></div>', width: 95},
 
           {field: "fechaEnvio", cellFilter: "date : 'dd/MM/yyyy'", displayName: 'F.Envio', width: 70},
           {field: "fechaDesembolso", cellFilter: "date : 'dd/MM/yyyy'", displayName: 'F.Desembolso', width: 70},
-          {field: "estado", displayName: 'Estado', width: 80},
+          {field: "estado", displayName: 'Estado', width: 95},
           {
             displayName: 'Edit',
-            width: 170,
-            cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><button type="button" class="btn btn-info btn-xs" ng-click="getVoucher(row.entity)" ng-disabled="getDisabledStateVoucher(row.entity)"><span class="glyphicon glyphicon-share"></span>Voucher</button>&nbsp;<button type="button" class="btn btn-success btn-xs" ng-click="confirmarTransaccion(row.entity)" ng-disabled="getDisabledStateButton(row.entity)"><span class="glyphicon glyphicon-ok"></span>Confirmar</button></div>'
+            width: 135,
+            cellTemplate:
+            '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;">' +
+
+            '<div class="btn-group dropdown">' +
+              '<button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">' +
+              'Button<span class="caret"></span>' +
+              '</button>' +
+              '<ul class="dropdown-menu">' +
+                '<li><a href="#">Action</a></li>' +
+                '<li><a href="#">Another action</a></li>' +
+                '<li><a href="#">Something else here</a></li>' +
+                '<li class="divider"></li>' +
+                '<li><a href="#">Separated link</a></li>' +
+              '</ul>' +
+            '</div>' +
+
+          '</div>'
           }]
       };
 
