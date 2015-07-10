3ea49671035756be5613dd1022479e6a43663bb7
Carlos <carlosthe19916@gmail.com>
2015-07-10 18:25:28 -0500

    sobre giros

--- SistCoopApp/src/main/webapp/scripts/controllers/caja/transaccion/CrearTransaccionSobreGiroController.js
+++ SistCoopApp/src/main/webapp/scripts/controllers/caja/transaccion/CrearTransaccionSobreGiroController.js
@@ -1,15 +1,28 @@
 define(['../../module'], function (controllers) {
   'use strict';
-  controllers.controller('CrearTransaccionSobreGiroController', ["$scope", "$state", "PersonaNaturalService", "AgenciaService", "SessionService", "MonedaService",
-    function ($scope, $state, PersonaNaturalService, AgenciaService, SessionService, MonedaService) {
+  controllers.controller('CrearTransaccionSobreGiroController', ["$scope", "$state", "PersonaNaturalService", "PersonaJuridicaService", "SessionService", "MaestroService",
+    function ($scope, $state, PersonaNaturalService, PersonaJuridicaService, SessionService, MaestroService) {
 
       $scope.control = {
         success: false,
         inProcess: false,
-        submitted : false
+        submitted: false
       };
 
+      $scope.combo = {
+        tipoPersonas: MaestroService.getTipoPersonas(),
+        tipoDocumentos: []
+      };
+
+      $scope.socioNatural = undefined;
+      $scope.socioJuridico = undefined;
+
       $scope.view = {
+        tipoPersona: undefined,
+        idTipoDocumento: undefined,
+        numeroDocumento: undefined,
+
+
         "numeroDocumentoEmisor": undefined,
         "nombreClienteEmisor": undefined,
         "numeroDocumentoReceptor": undefined,
@@ -25,178 +38,85 @@ define(['../../module'], function (controllers) {
         total: '0'
       };
 
-      $scope.transaccion = {};
-
-      $scope.$watch('view.monto', function (newValue, oldValue) {
-        $scope.updateTotal();
-      }, true);
-      $scope.$watch('view.comision', function (newValue, oldValue) {
-        $scope.updateTotal();
-      }, true);
-      $scope.$watch('view.tipoComision', function (newValue, oldValue) {
-        $scope.updateTotal();
-      }, true);
-      $scope.$watch('view.modoPagoComision', function (newValue, oldValue) {
-        $scope.updateTotal();
-      }, true);
-      $scope.$watch('view.lugarPagoComision', function (newValue, oldValue) {
-        $scope.updateTotal();
-      }, true);
-
-      $scope.updateTotal = function () {
-
-        var monto = 0;
-        var comision = 0;
-        var total = 0;
-        var estadoPagoComision = false;
-
-        //hallando monto
-        if ($scope.view.modoPagoComision == 'ANADIR') {
-          monto = parseFloat($scope.view.monto);
-          //redondeo a un decimal y hacia arriba
-          monto = Math.round(monto * 10) / 10;
-        } else {
-          if ($scope.view.tipoComision == 'PORCENTURAL') {
-            monto = (100 * parseFloat($scope.view.monto)) / (100 + parseFloat($scope.view.comision));
-            //redondeo a un decimal y hacia abajo
-            monto = Math.floor(monto * 10) / 10;
+      $scope.$watch('view.tipoPersona', function (newValue, oldValue) {
+        if ($scope.view.tipoPersona) {
+          $scope.socioNatural = undefined;
+          $scope.socioJuridico = undefined;
+          if ($scope.view.tipoPersona == "NATURAL") {
+            PersonaNaturalService.getTipoDocumentos().then(function (data) {
+              $scope.combo.tipoDocumentos = data;
+            });
           } else {
-            monto = parseFloat($scope.view.monto) - parseFloat($scope.view.comision);
+            if ($scope.view.tipoPersona == "JURIDICA") {
+              PersonaJuridicaService.getTipoDocumentos().then(function (data) {
+                $scope.combo.tipoDocumentos = data;
+              });
+            }
           }
         }
+      }, true);
 
-        //hallando comision
-        if ($scope.view.modoPagoComision == 'ANADIR') {
-          if ($scope.view.tipoComision == 'PORCENTURAL') {
-            comision = parseFloat($scope.view.monto) * parseFloat($scope.view.comision) / 100;
-            //redondeo a un decimal y hacia arriba
-            comision = Math.round(comision * 10) / 10;
-          } else {
-            comision = parseFloat($scope.view.comision);
-          }
-        } else {
-          comision = parseFloat($scope.view.monto).toFixed(2) - parseFloat(monto).toFixed(2);
-          comision = comision.toFixed(2);
+      $scope.buscarPersonaSocio = function ($event) {
+        if (angular.isUndefined($scope.view.idTipoDocumento) || angular.isUndefined($scope.view.numeroDocumento)) {
+          if (!angular.isUndefined($event))
+            $event.preventDefault();
+          return;
         }
 
-        //estado de pago comision
-        if ($scope.view.lugarPagoComision == 'AL_ENVIAR') {
-          estadoPagoComision = true;
-
-          total = parseFloat(monto) + parseFloat(comision);
-          total = total.toFixed(2);
-        } else {
-          estadoPagoComision = false;
-
-          total = parseFloat(monto).toFixed(2);
-        }
-
-        $scope.transaccion = {
-          "monto": monto,
-          "comision": comision,
-          "total": total,
-          "estadoPagoComision": estadoPagoComision
-        };
-
-      };
-
-      $scope.objetosCargados = {
-        agenciaOrigen: undefined
-      };
-
-      $scope.combo = {
-        moneda: undefined,
-        agencia: undefined
-      };
-
-      $scope.loadAgenciaOrigen = function () {
-        $scope.objetosCargados.agenciaOrigen = $scope.agenciaSession;
-        $scope.view.idAgenciaOrigen = $scope.agenciaSession.id;
-      };
-      $scope.loadAgenciasDestino = function () {
-        AgenciaService.getAgencias(true).then(function (data) {
-          $scope.combo.agencia = data;
-        });
-      };
-      $scope.loadAgenciaOrigen();
-      $scope.loadAgenciasDestino();
-
-      $scope.loadMonedas = function () {
-        MonedaService.getMonedas().then(function (data) {
-          $scope.combo.moneda = data;
-        });
-      };
-      $scope.loadMonedas();
-
-      //
-      $scope.getTipoMoneda = function () {
-        if (!angular.isUndefined($scope.view.idMoneda) && !angular.isUndefined($scope.combo.moneda)) {
-          for (var i = 0; i < $scope.combo.moneda.length; i++)
-            if ($scope.view.idMoneda == $scope.combo.moneda[i].id)
-              return $scope.combo.moneda[i];
-        } else {
-          return undefined;
-        }
-      };
-
-      $scope.buscarClienteEmisor = function ($event) {
-        if (!angular.isUndefined($event)) {
-          $event.preventDefault();
-        }
-        PersonaNaturalService.findByTipoNumeroDocumento(1, $scope.view.numeroDocumentoEmisor).then(function (data) {
-          $scope.view.nombreClienteEmisor = data.apellidoPaterno + " " + data.apellidoMaterno + " " + data.nombres;
-        });
-      };
-      $scope.buscarClienteReceptor = function ($event) {
-        if (!angular.isUndefined($event)) {
-          $event.preventDefault();
-        }
-        PersonaNaturalService.findByTipoNumeroDocumento(1, $scope.view.numeroDocumentoReceptor).then(function (data) {
-          $scope.view.nombreClienteReceptor = data.apellidoPaterno + " " + data.apellidoMaterno + " " + data.nombres;
-        });
-      };
-
-      //transaccion
-      $scope.crearTransaccion = function () {
-
-        if ($scope.form.$valid) {
-
-          var transaccion = {
-            "idAgenciaOrigen": $scope.view.idAgenciaOrigen,
-            "idAgenciaDestino": $scope.view.idAgenciaDestino,
-
-            "numeroDocumentoEmisor": $scope.view.numeroDocumentoEmisor,
-            "clienteEmisor": $scope.view.nombreClienteEmisor,
-            "numeroDocumentoReceptor": $scope.view.numeroDocumentoReceptor,
-            "clienteReceptor": $scope.view.nombreClienteReceptor,
-
-            "idMoneda": $scope.view.idMoneda,
-            "lugarPagoComision": $scope.view.lugarPagoComision,
-            "monto": $scope.transaccion.monto,
-            "comision": $scope.transaccion.comision,
-            "estadoPagoComision": $scope.transaccion.estadoPagoComision
-          };
-
-          SessionService.crearTransaccionGiro(transaccion).then(
-            function (data) {
-              $state.transitionTo('app.transaccion.editarGiro', {id: data.id});
-            },
-            function error(error) {
-              $scope.alerts = [{type: "danger", msg: "Error: " + error.data.message + "."}];
+        var tipoDoc = $scope.view.idTipoDocumento;
+        var numDoc = $scope.view.numeroDocumento;
+        if ($scope.view.tipoPersona == "NATURAL") {
+          PersonaNaturalService.findByTipoNumeroDocumento(tipoDoc, numDoc).then(function (data) {
+            $scope.socioNatural = data;
+            if (angular.isUndefined(data) || data === null) {
+              $scope.socioNatural = undefined;
+              $scope.alerts = [{type: "danger", msg: "Persona No Encontrada."}];
+              $scope.closeAlert = function (index) {
+                $scope.alerts.splice(index, 1);
+              };
+            } else {
+              $scope.alerts = [{type: "success", msg: "Persona (Socio) Encontrada."}];
               $scope.closeAlert = function (index) {
                 $scope.alerts.splice(index, 1);
               };
             }
-          );
+
+          }, function error(error) {
+            $scope.socioNatural = undefined;
+            $scope.alerts = [{type: "danger", msg: "Error al buscar la persona."}];
+            $scope.closeAlert = function (index) {
+              $scope.alerts.splice(index, 1);
+            };
+          });
         } else {
-          $scope.control.submitted = true;
+          if ($scope.view.tipoPersona == "JURIDICA") {
+            PersonaJuridicaService.findByTipoNumeroDocumento(tipoDoc, numDoc).then(function (persona) {
+              $scope.socioJuridico = persona;
+              if (angular.isUndefined(persona) || persona === null) {
+                $scope.socioJuridico = undefined;
+                $scope.alerts = [{type: "danger", msg: "Persona Jur&iacute;dica No Encontrada."}];
+                $scope.closeAlert = function (index) {
+                  $scope.alerts.splice(index, 1);
+                };
+              } else {
+                $scope.alerts = [{type: "success", msg: "Persona (Socio) Encontrada."}];
+                $scope.closeAlert = function (index) {
+                  $scope.alerts.splice(index, 1);
+                };
+              }
+            }, function error(error) {
+              $scope.socioJuridico = undefined;
+              $scope.alerts = [{type: "danger", msg: "Error al buscar la persona."}];
+              $scope.closeAlert = function (index) {
+                $scope.alerts.splice(index, 1);
+              }
+            });
+          }
         }
-
+        if ($event !== undefined)
+          $event.preventDefault();
       };
 
-      $scope.cancelar = function () {
-        $state.transitionTo("app.transaccion.buscarGiros");
-      };
 
     }]);
 });
