b38f4866bc8e95d535fe289bd7de2cf0b06b05d0
Carlos <carlosthe19916@gmail.com>
2015-09-07 19:07:02 -0500

    ver saldos de cajas

--- SistCoopJefeCajaApp/src/main/webapp/scripts/controllers/caja/BuscarCajaController.js
+++ SistCoopJefeCajaApp/src/main/webapp/scripts/controllers/caja/BuscarCajaController.js
@@ -1,43 +1,60 @@
 define(['../module'], function (controllers) {
-    'use strict';
-    controllers.controller('BuscarCajaController', ['$scope','$state','focus','CajaService',
-        function($scope,$state,focus,CajaService) {
+  'use strict';
+  controllers.controller('BuscarCajaController', ['$scope', '$state', 'focus', 'CajaService',
+    function ($scope, $state, focus, CajaService) {
 
-            $scope.setInitialFocus = function($event){
-                if(!angular.isUndefined($event))
-                    $event.preventDefault();
-                focus('focusFilterText');
-            };
-            $scope.setInitialFocus();
+      $scope.setInitialFocus = function ($event) {
+        if (!angular.isUndefined($event))
+          $event.preventDefault();
+        focus('focusFilterText');
+      };
+      $scope.setInitialFocus();
 
 
-            $scope.nuevo = function(){
-                $state.transitionTo('app.caja.nuevaCaja');
-            };
+      $scope.nuevo = function () {
+        $state.transitionTo('app.caja.nuevaCaja');
+      };
 
-            $scope.loadCajas = function(){
-                CajaService.getCajas($scope.agenciaSession.id).then(function(data){
-                    $scope.cajas = data;
-                });
-            };
-            $scope.loadCajas();
+      $scope.loadCajas = function () {
+        CajaService.getCajas($scope.agenciaSession.id).then(function (data) {
+          $scope.cajas = data;
+        });
+      };
+      $scope.loadCajas();
 
-            $scope.gridOptions = {
-                data: 'cajas',
-                multiSelect: false,
-                columnDefs: [
-                    { field: "denominacion", displayName: "DENOMINACION" , width: 100},
-                    {displayName: 'ABIERTO/CERRADO', width: 115, cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.abierto">ABIERTO</span><span ng-hide="row.entity.abierto">CERRADO</span></div>'},
-                    {displayName: 'MOVIMIENTO', width: 110, cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estadoMovimiento">DESCONGELADO</span><span ng-hide="row.entity.estadoMovimiento">CONGELADO</span></div>'},
-                    { field: "bovedas", displayName: "BOVEDAS"},
-                    {displayName: 'ESTADO', width: 80, cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estado">ACTIVO</span><span ng-hide="row.entity.estado">INACTIVO</span></div>'},
-                    {displayName: 'EDIT',  width: 150, cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><button type="button" class="btn btn-info btn-xs" ng-click="editar(row.entity)"><span class="glyphicon glyphicon-share"></span>Edit</button></div>'}
-                ]
-            };
+      $scope.gridOptions = {
+        data: 'cajas',
+        multiSelect: false,
+        columnDefs: [
+          {field: "denominacion", displayName: "DENOMINACION", width: '15%'},
+          {
+            displayName: 'ABIERTO/CERRADO',
+            cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.abierto">ABIERTO</span><span ng-hide="row.entity.abierto">CERRADO</span></div>',
+            width: '12%'
+          },
+          {
+            displayName: 'MOVIMIENTO',
+            cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estadoMovimiento">DESCONGELADO</span><span ng-hide="row.entity.estadoMovimiento">CONGELADO</span></div>',
+            width: '12%'
+          },
+          {field: "bovedas", displayName: "BOVEDAS", width: '23%'},
+          {field: "saldos", displayName: "SALDOS", width: '18%'},
+          {
+            displayName: 'ESTADO',
+            cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estado">ACTIVO</span><span ng-hide="row.entity.estado">INACTIVO</span></div>',
+            width: '10%'
+          },
+          {
+            displayName: 'EDIT',
+            cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><button type="button" class="btn btn-info btn-xs" ng-click="editar(row.entity)"><span class="glyphicon glyphicon-share"></span>Edit</button></div>',
+            width: '9%'
+          }
+        ]
+      };
 
-            $scope.editar = function(boveda) {
-                $state.transitionTo('app.caja.editarCaja', { id: boveda.id });
-            };
+      $scope.editar = function (boveda) {
+        $state.transitionTo('app.caja.editarCaja', {id: boveda.id});
+      };
 
-        }]);
+    }]);
 });
--- SistCoopREST/src/main/java/org/sistemafinanciero/entity/dto/CajaView.java
+++ SistCoopREST/src/main/java/org/sistemafinanciero/entity/dto/CajaView.java
@@ -3,10 +3,12 @@ package org.sistemafinanciero.entity.dto;
 import java.io.Serializable;
 import java.math.BigInteger;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -14,113 +16,163 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import org.hibernate.annotations.Subselect;
 
-@Entity
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.PROPERTY)
-@Subselect(value = "SELECT c.ID_CAJA as idCaja, "
-		+ "c.denominacion, "
-		+ "c.abreviatura, "
-		+ "c.estado, "
-		+ "c.abierto, "
-		+ "c.ESTADO_MOVIMIENTO as estadoMovimiento, "
-		+ "listagg(b.denominacion || ',') WITHIN GROUP (ORDER BY c.ID_CAJA) AS bovedas, "
-		+ "a.id_agencia as idAgencia "
-		+ "FROM Caja c "
-		+ "INNER JOIN BOVEDA_CAJA bc ON C.id_caja = bc.id_caja "
-		+ "INNER JOIN BOVEDA b ON B.id_boveda = bc.id_boveda "
-		+ "INNER JOIN AGENCIA a ON a.id_agencia = b.id_agencia "
-		+ "GROUP BY c.ID_CAJA, c.denominacion, c.abreviatura, c.estado, c.abierto, c.ESTADO_MOVIMIENTO, a.id_agencia")
+/*@Subselect(value = "SELECT c.ID_CAJA as idCaja, "
+ + "c.denominacion, "
+ + "c.abreviatura, "
+ + "c.estado, "
+ + "c.abierto, "
+ + "c.ESTADO_MOVIMIENTO as estadoMovimiento, "
+ + "listagg(b.denominacion || ',') WITHIN GROUP (ORDER BY c.ID_CAJA) AS bovedas, "
+ + "a.id_agencia as idAgencia "
+ + "FROM Caja c "
+ + "INNER JOIN BOVEDA_CAJA bc ON C.id_caja = bc.id_caja "
+ + "INNER JOIN BOVEDA b ON B.id_boveda = bc.id_boveda "
+ + "INNER JOIN AGENCIA a ON a.id_agencia = b.id_agencia "
+ + "GROUP BY c.ID_CAJA, c.denominacion, c.abreviatura, c.estado, c.abierto, c.ESTADO_MOVIMIENTO, a.id_agencia")*/
 @NamedQueries({ @NamedQuery(name = CajaView.findByIdAgencia, query = "SELECT c FROM CajaView c WHERE c.idAgencia = :idAgencia") })
+@Entity
+@Table(name = "CAJA_VIEW", schema = "C##BDSISTEMAFINANCIERO")
+@XmlRootElement(name = "cajaView")
+@XmlAccessorType(XmlAccessType.NONE)
 public class CajaView implements Serializable {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public final static String findByIdAgencia = "CajaView.findByIdAgencia";
+    public final static String findByIdAgencia = "CajaView.findByIdAgencia";
 
-	private BigInteger idCaja;
-	private String denominacion;
-	private String abreviatura;
-	private int estado;
-	private int abierto;
-	private int estadoMovimiento;
-	
-	private String bovedas;
-	
-	private BigInteger idAgencia;
+    private BigInteger idCaja;
+    private String denominacion;
+    private String abreviatura;
+    private int estado;
+    private int abierto;
+    private int estadoMovimiento;
 
-	// private Set bovedaCajas = new HashSet(0);
+    private String bovedas;
+    private String saldos;
 
-	public CajaView() {
-		// TODO Auto-generated constructor stub
-	}
+    private BigInteger idAgencia;
 
-	@XmlElement(name = "id")
-	@Id
-	public BigInteger getIdCaja() {
-		return idCaja;
-	}
+    public CajaView() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public void setIdCaja(BigInteger idCaja) {
-		this.idCaja = idCaja;
-	}
+    @XmlElement(name = "id")
+    @Id
+    @Column(name = "ID_CAJA", unique = true, nullable = false, precision = 22, scale = 0)
+    public BigInteger getIdCaja() {
+        return idCaja;
+    }
 
-	public String getDenominacion() {
-		return denominacion;
-	}
+    public void setIdCaja(BigInteger idCaja) {
+        this.idCaja = idCaja;
+    }
 
-	public void setDenominacion(String denominacion) {
-		this.denominacion = denominacion;
-	}
+    @XmlElement(name = "denominacion")
+    @Column(name = "DENOMINACION", nullable = false, length = 100, columnDefinition = "nvarchar2")
+    public String getDenominacion() {
+        return denominacion;
+    }
 
-	public String getAbreviatura() {
-		return abreviatura;
-	}
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
 
-	public void setAbreviatura(String abreviatura) {
-		this.abreviatura = abreviatura;
-	}
+    @XmlElement(name = "abreviatura")
+    @Column(name = "ABREVIATURA", nullable = false, length = 100, columnDefinition = "nvarchar2")
+    public String getAbreviatura() {
+        return abreviatura;
+    }
 
-	public boolean getEstado() {
-		return estado == 1;
-	}
+    public void setAbreviatura(String abreviatura) {
+        this.abreviatura = abreviatura;
+    }
 
-	public void setEstado(boolean estado) {
-		this.estado = (estado ? 1: 0);
-	}
+    @XmlElement(name = "estado")
+    @Column(name = "ESTADO", nullable = false, precision = 22, scale = 0)
+    public boolean getEstado() {
+        return estado == 1;
+    }
 
-	public boolean getAbierto() {
-		return abierto == 1;
-	}
+    public void setEstado(boolean estado) {
+        this.estado = (estado ? 1 : 0);
+    }
 
-	public void setAbierto(boolean abierto) {
-		this.abierto = (abierto ? 1 : 0);
-	}
+    @XmlElement(name = "abierto")
+    @Column(name = "ABIERTO", nullable = false, precision = 22, scale = 0)
+    public boolean getAbierto() {
+        return abierto == 1;
+    }
 
-	public boolean getEstadoMovimiento() {
-		return estadoMovimiento == 1;
-	}
+    public void setAbierto(boolean abierto) {
+        this.abierto = (abierto ? 1 : 0);
+    }
 
-	public void setEstadoMovimiento(boolean estadomovimiento) {
-		this.estadoMovimiento = (estadomovimiento ? 1 : 0);
-	}
+    @XmlElement(name = "estadoMovimiento")
+    @Column(name = "ESTADO_MOVIMIENTO", nullable = false, precision = 22, scale = 0)
+    public boolean getEstadoMovimiento() {
+        return estadoMovimiento == 1;
+    }
 
-	public String getBovedas() {
-		return bovedas;
-	}
+    public void setEstadoMovimiento(boolean estadomovimiento) {
+        this.estadoMovimiento = (estadomovimiento ? 1 : 0);
+    }
 
-	public void setBovedas(String bovedas) {
-		this.bovedas = bovedas;
-	}
+    @XmlElement(name = "bovedas")
+    @Column(name = "BOVEDAS", nullable = false, length = 100)
+    public String getBovedas() {
+        return bovedas;
+    }
 
-	public BigInteger getIdAgencia() {
-		return idAgencia;
-	}
+    public void setBovedas(String bovedas) {
+        this.bovedas = bovedas;
+    }
 
-	public void setIdAgencia(BigInteger idAgencia) {
-		this.idAgencia = idAgencia;
-	}
+    @XmlElement(name = "idAgencia")
+    @Column(name = "ID_AGENCIA", precision = 22, scale = 0)
+    public BigInteger getIdAgencia() {
+        return idAgencia;
+    }
+
+    public void setIdAgencia(BigInteger idAgencia) {
+        this.idAgencia = idAgencia;
+    }
+
+    @XmlElement(name = "saldos")
+    @Column(name = "SALDOS", nullable = false, length = 100)
+    public String getSaldos() {
+        return saldos;
+    }
+
+    public void setSaldos(String saldos) {
+        this.saldos = saldos;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((idCaja == null) ? 0 : idCaja.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CajaView other = (CajaView) obj;
+        if (idCaja == null) {
+            if (other.idCaja != null)
+                return false;
+        } else if (!idCaja.equals(other.idCaja))
+            return false;
+        return true;
+    }
 
 }
