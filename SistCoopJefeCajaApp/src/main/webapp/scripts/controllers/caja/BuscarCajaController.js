db64146e52300d76b9c82eefe7b9007dbe77ce83
Jhon <Jhon@Jhon-PC>
2015-10-07 16:21:20 -0500

    voucher compra venta

--- SistCoopJefeCajaApp/src/main/webapp/scripts/controllers/caja/BuscarCajaController.js
+++ SistCoopJefeCajaApp/src/main/webapp/scripts/controllers/caja/BuscarCajaController.js
@@ -26,7 +26,7 @@ define(['../module'], function (controllers) {
         data: 'cajas',
         multiSelect: false,
         columnDefs: [
-          {field: "denominacion", displayName: "DENOMINACION", width: '15%'},
+          {field: "denominacion", displayName: "DENOMINACION", width: '10%'},
           {
             displayName: 'ABIERTO/CERRADO',
             cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.abierto">ABIERTO</span><span ng-hide="row.entity.abierto">CERRADO</span></div>',
@@ -35,19 +35,19 @@ define(['../module'], function (controllers) {
           {
             displayName: 'MOVIMIENTO',
             cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estadoMovimiento">DESCONGELADO</span><span ng-hide="row.entity.estadoMovimiento">CONGELADO</span></div>',
-            width: '12%'
+            width: '10%'
           },
-          {field: "bovedas", displayName: "BOVEDAS", width: '23%'},
-          {field: "saldos", displayName: "SALDOS", width: '18%'},
+          {field: "bovedas", displayName: "BOVEDAS", width: '25%'},
+          {field: "saldos", displayName: "SALDOS", width: '20%'},
           {
             displayName: 'ESTADO',
             cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><span ng-show="row.entity.estado">ACTIVO</span><span ng-hide="row.entity.estado">INACTIVO</span></div>',
-            width: '10%'
+            width: '8%'
           },
           {
             displayName: 'EDIT',
             cellTemplate: '<div ng-class="col.colIndex()" class="ngCellText ng-scope col6 colt6" style="text-align: center;"><button type="button" class="btn btn-info btn-xs" ng-click="editar(row.entity)"><span class="glyphicon glyphicon-share"></span>Edit</button></div>',
-            width: '9%'
+            width: '7%'
           }
         ]
       };
